994934a105b6dd83161d79f024fbb9416b04edec
brause06 <brause06@gmail.com>
2024-11-13 00:15:26 -0300

    finalizo completar mision

--- src/models/mision.py
+++ src/models/mision.py
@@ -11,6 +11,7 @@ class Mision:
         self.__es_grupal = es_grupal
         self.__min_miembros = min_miembros if es_grupal else 1
         self.__completada = False
+        self.__participantes: List[Aventurero] = []
 
     @property
     def nombre(self):
@@ -59,6 +60,49 @@ class Mision:
     @completada.setter
     def completada(self, value):
         self.__completada = value
+    
+    @property
+    def participantes(self):
+        return self.__participantes
+    
+    @participantes.setter
+    def participantes(self, value):
+        self.__participantes = value
+
+
+    
 
     def completar_mision(self, aventureros: List[Aventurero]) -> bool:
-        pass
+        if not self.puede_ser_completada(aventureros):
+            return False
+        self.completada(True)
+        self.participantes(aventureros)
+
+        #Calcula recompensa
+        recompensa_por_participante = self.recompensa / len(aventureros)
+
+        # Tabla de experiencia según el rango de la misión
+        recompensa_experiencia = {
+            1: 5,    # Misión rango 1: 5 puntos de experiencia
+            2: 10,   # Misión rango 2: 10 puntos de experiencia
+            3: 20,   # Misión rango 3: 20 puntos de experiencia
+            4: 50,   # Misión rango 4: 50 puntos de experiencia
+            5: 100   # Misión rango 5: 100 puntos de experiencia
+        }
+        experiencia = recompensa_experiencia[self.ranking]
+
+        # Distribuye recompensas y experiencia a cada participante
+        for aventurero in aventureros:
+            aventurero.agregar_dinero(recompensa_por_participante)
+            aventurero.agregar_experiencia(experiencia)
+            aventurero.agregar_mision_completada(self)
+
+        return True
+
+
+
+
+    def puede_ser_completada(self, aventureros: List[Aventurero]) -> bool:
+        if len(aventureros) < self.min_miembros:
+            return False
+        return all(aventurero.calcular_rango() >= self.ranking for aventurero in aventureros)
